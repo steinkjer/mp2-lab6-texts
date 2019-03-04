43d46cf5c85ec5e459ab48a0ca8df1e10223d8ca
Anton Shtanyuk <ashtanyuk@gmail.com>
2019-03-04 21:34:11 +0300

    Initial commit

---
+++ samples/main.cpp
@@ -0,0 +1,14 @@
+// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+//
+// Copyright (c) Гергель В.П. 28.07.2000
+//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (21.04.2015)
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
